694143ee0d487387db882d00c6b95cdd63e00f0b
MaryKhm656 <nanoman66656@gmail.com>
2025-11-04 11:06:20 +0300

    Added logic for getting all participant by game

--- app/service/participant_service.py
+++ app/service/participant_service.py
@@ -1,6 +1,8 @@
+from typing import List
+
 from sqlalchemy.orm import Session
 
-from app.db.models import Participant
+from app.db.models import Participant, Game, User
 
 
 class ParticipantService:
@@ -26,3 +28,18 @@ class ParticipantService:
         db.refresh(participant)
 
         return participant
+
+    @staticmethod
+    def get_all_participant_by_game(db: Session, game_id: int, organizer_id: int) -> List[Participant]:
+        game = db.get(Game, game_id)
+        if not game:
+            raise ValueError("Игра не найдена")
+
+        organizer = db.get(User, organizer_id)
+        if not organizer:
+            raise ValueError("Пользователь не найден")
+
+        if game.organizer_id != organizer.id:
+            raise ValueError("Данные действия доступны только организатору игры")
+
+        return db.query(Participant).filter(Participant.game_id==game.id).all()
